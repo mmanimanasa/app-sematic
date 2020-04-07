4e160510e1fb5276f75f1e1803aeffe0137d6072
Manasa Mandadapu, M <m.manasa.mandadapu@accenture.com>
2020-04-08 03:37:37 +0530

    Updates

---
+++ precache-manifest.711072c4f6daaaa16a0b1f000b547bbe.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00b5038c462ec28ab12b1518ca24f64",
+    "url": "/app-sematic/index.html"
+  },
+  {
+    "revision": "835733bb70f305bd464b",
+    "url": "/app-sematic/static/css/2.9e01c58b.chunk.css"
+  },
+  {
+    "revision": "0e09fbb1647bcb9afcd0",
+    "url": "/app-sematic/static/css/main.72e05502.chunk.css"
+  },
+  {
+    "revision": "835733bb70f305bd464b",
+    "url": "/app-sematic/static/js/2.c7594abe.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/app-sematic/static/js/2.c7594abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e09fbb1647bcb9afcd0",
+    "url": "/app-sematic/static/js/main.11d41a01.chunk.js"
+  },
+  {
+    "revision": "718ae3d6fafa1d0083c0",
+    "url": "/app-sematic/static/js/runtime-main.1a6db880.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/app-sematic/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/app-sematic/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/app-sematic/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/app-sematic/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/app-sematic/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/app-sematic/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/app-sematic/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/app-sematic/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/app-sematic/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/app-sematic/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/app-sematic/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-sematic/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/app-sematic/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/app-sematic/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/app-sematic/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/app-sematic/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/app-sematic/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
